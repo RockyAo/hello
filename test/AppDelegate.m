a88e7e1a1f326fc86091368789b795ea722213b8
RockyAo <542622608@qq.com>
2015-12-07 16:16:07 +0800

    asdf

--- test/AppDelegate.m
+++ test/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "rockstar/swift_rockstar.py"
 
 @interface AppDelegate ()
 
--- test/ViewController.m
+++ test/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "rockstar/RockStar.py"
 
 @interface ViewController ()
 
